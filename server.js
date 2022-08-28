b83b78c86fd823b3874141354591689609034656
guinnod <210103064@stu.sdu.edu.kz>
2022-08-28 19:49:59 +0600

    cors

--- server.js
+++ server.js
@@ -24,8 +24,6 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(cors({
-    origin: "http://localhost:3000",
-    methods: "GET,POST,PUT,DELETE",
     credentials: true,
 }));
 
